e24be02b3126964bad715a829c7ea65cac084e98
Jose Maria <josemariagodoy1996@gmail.com>
2026-03-09 19:49:41 +0100

    <feat>(AgeChecker.java): creo el archivo del ejercicio

---
+++ java-basics/03-control-flow/AgeChecker.java
@@ -0,0 +1,26 @@
+/*
+Exercise: Age checker
+
+Goal:
+Practice conditional statements.
+
+Instructions:
+1. Ask the user for their age.
+2. Read the age as an integer.
+3. If the age is 18 or greater print:
+
+	"You are an adult"
+
+4. Otherwise print:
+
+	"You are a minor"
+
+Example:
+
+Enter your age: 20
+You are an adult
+*/
+
+public class AgeChecker {
+	
+}
