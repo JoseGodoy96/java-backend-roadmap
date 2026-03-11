73766275343f0bdd07b0205fc99f4cfd786fed6a
Jose Maria <josemariagodoy1996@gmail.com>
2026-03-11 22:33:52 +0100

    <feat>(DynamicSUm.java): Inicio del ejercicio

---
+++ java-basics/06-collections/DynamicSum.java
@@ -0,0 +1,25 @@
+/*
+Exercise: Dynamic Number List
+
+Goal:
+Learn to use ArrayList instead of fixed-size arrays.
+
+Instructions:
+
+1. Ask the user how many numbers they want to enter.
+2. Create an ArrayList to store the numbers.
+3. Read the numbers from the user.
+4. Store them in the list.
+5. Calculate the sum of all numbers.
+6. Print the total.
+
+Concepts practiced:
+- ArrayList
+- dynamic collections
+- loops
+- accumulation
+*/
+
+public class DynamicSum {
+	
+}
