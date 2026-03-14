1c0708728567f90172dea6177ddfd92a9633c29e
Jose Maria <josemariagodoy1996@gmail.com>
2026-03-14 10:41:42 +0100

    <feat>(DynamicSUm.java): Final del ejercicio

--- java-basics/06-collections/DynamicSum.java
+++ java-basics/06-collections/DynamicSum.java
@@ -20,6 +20,39 @@ Concepts practiced:
 - accumulation
 */
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class DynamicSum {
 	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Cuantos numeros vas a introducir? ");
+		int total = sc.nextInt();
+
+		ArrayList<Integer> numbers = new ArrayList<>();
+
+		int i = 0;
+		while (i < total) {
+			System.out.print("Introduzca el numero: ");
+			int num = sc.nextInt();
+			numbers.add(num);
+			i++;
+		}
+
+		System.out.println("Todos los numeros introducidos!");
+
+		i = 0;
+		int sum = 0;
+
+		while (i < numbers.size()) {
+			sum += numbers.get(i);
+			i++;
+		}
+
+		System.out.println("La suma total es: " + sum);
+
+		sc.close();
+	}
 }
