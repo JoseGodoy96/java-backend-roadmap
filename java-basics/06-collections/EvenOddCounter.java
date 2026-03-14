5b1930b37d30bb47e92538a3c7b2364304734184
Jose Maria <josemariagodoy1996@gmail.com>
2026-03-14 10:48:18 +0100

    <feat>(EvenOddCounter.java): Inicion del ejercicio

---
+++ java-basics/06-collections/EvenOddCounter.java
@@ -0,0 +1,41 @@
+/*
+Exercise: Even and Odd Counter
+
+Goal:
+Practice working with ArrayList and conditions.
+
+Instructions:
+
+1. Ask the user how many numbers they want to enter.
+2. Store the numbers in an ArrayList.
+3. Count how many numbers are:
+	- even
+	- odd
+4. Print the results.
+
+Example:
+
+Enter how many numbers: 5
+
+Enter number: 2
+Enter number: 3
+Enter number: 4
+Enter number: 7
+Enter number: 10
+
+Even numbers: 3
+Odd numbers: 2
+
+Concepts practiced:
+- ArrayList
+- loops
+- conditions
+- modulo operator (%)
+*/
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class EvenOddCounter(){
+	
+}
