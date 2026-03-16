e29c886a0f59025400f95261660c6d9555823df9
Jose Maria <josemariagodoy1996@gmail.com>
2026-03-16 23:13:44 +0100

    <feat>(exercise-3, OldestUserFinder.java): Inicio del ejercicio

---
+++ java-basics/07-oop/exercise-3/OldestUserFinder.java
@@ -0,0 +1,41 @@
+/*
+Exercise: Find the Oldest User
+
+Goal:
+Practice iterating over objects and finding a maximum value.
+
+Instructions:
+
+1. Use the User class.
+
+2. Ask the user how many users they want to enter.
+
+3. Store all users in an ArrayList.
+
+4. After all users are entered:
+
+	- Find the user with the highest age.
+
+5. Print:
+
+	"Oldest user: NAME - AGE"
+
+Example:
+
+How many users? 3
+
+Ana 20
+Carlos 35
+Marta 28
+
+Oldest user: Carlos - 35
+
+Concepts practiced:
+- ArrayList of objects
+- comparison
+- tracking a maximum value
+*/
+
+public class OldestUserFinder {
+	
+}
