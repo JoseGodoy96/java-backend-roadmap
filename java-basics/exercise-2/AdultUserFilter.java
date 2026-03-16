6ea2571190929e3313a353d3bbe42deb529aa487
Jose Maria <josemariagodoy1996@gmail.com>
2026-03-16 21:35:28 +0100

    <feat>(Exercise-1, Exercise-2, AdultUserFilter.java): Decido crear carpetas por ejercicios para que sea mas identificable cada ejercicio

---
+++ java-basics/exercise-2/AdultUserFilter.java
@@ -0,0 +1,53 @@
+/*
+Exercise: Filter Adult Users
+
+Goal:
+Practice working with lists of objects and filtering data.
+
+Instructions:
+
+1. Use the User class created in the previous exercise.
+
+2. In the main program:
+
+	- Ask the user how many users they want to enter.
+	- Create an ArrayList to store User objects.
+
+3. For each user:
+	- Ask for the name
+	- Ask for the age
+	- Create a new User object
+	- Add it to the list
+
+4. After all users are entered:
+
+	- Print ONLY the users who are adults (age >= 18).
+
+5. If there are no adult users, print:
+
+	"No adult users found"
+
+Example interaction:
+
+How many users? 3
+
+Enter name: Ana
+Enter age: 15
+
+Enter name: Carlos
+Enter age: 22
+
+Enter name: Marta
+Enter age: 30
+
+Adult users:
+
+Carlos - 22
+Marta - 30
+
+Concepts practiced:
+- ArrayList of objects
+- object attribute access
+- filtering data with conditions
+- loops
+*/
