0e1efbb2372f4e99fc36192cd188d366ceea32fa
Jose Maria <josemariagodoy1996@gmail.com>
2026-03-17 20:07:01 +0100

    <feat>(exercise-4, UserSearch.java): Inicio del ejercicio

---
+++ java-basics/07-oop/exercise-4/UserSearch.java
@@ -0,0 +1,37 @@
+/*
+Exercise: Search User by Name
+
+Goal:
+Practice searching objects inside a list.
+
+Instructions:
+
+1. Use the User class.
+
+2. Ask the user how many users they want to enter.
+
+3. Store all users in an ArrayList.
+
+4. Ask the user for a name to search.
+
+5. Search the list:
+
+	- If a user with that name exists → print it
+	- If not → print "User not found"
+
+Example:
+
+Users:
+Ana 20
+Carlos 30
+
+Search: Carlos
+
+Result:
+Carlos - 30
+
+Concepts practiced:
+- searching in collections
+- string comparison
+- loops
+*/
