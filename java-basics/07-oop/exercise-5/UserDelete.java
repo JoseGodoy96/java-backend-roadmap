5da60e9fd38a0da931d75d4f69428dd8fdcebe3a
Jose Maria <josemariagodoy1996@gmail.com>
2026-03-21 11:09:31 +0100

    <feat>(User.java, UserDelete.java): Final del ejercicio

--- java-basics/07-oop/exercise-5/UserDelete.java
+++ java-basics/07-oop/exercise-5/UserDelete.java
@@ -43,6 +43,68 @@ Concepts practiced:
 - object comparison
 */
 
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserDelete {
-	
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		List<User> users = new ArrayList<>();
+
+		System.out.print("Cuantos usuarios vas a introducir? ");
+		int total = sc.nextInt();
+		sc.nextLine();
+
+		if (total == 0) {
+			System.out.print("No hay usuarios");
+			sc.close();
+			return ;
+		}
+
+		while (total > 0)
+		{
+			System.out.print("Como se llama? ");
+			String name = sc.nextLine();
+
+			System.out.print("Cual es su edad? ");
+			int age = sc.nextInt();
+			sc.nextLine();
+
+			users.add(new User(name, age));
+			total--;
+		}
+
+		System.out.print("Como se llama el usuario que vas a borrar? ");
+		String searchName = sc.nextLine().toLowerCase();
+
+		int i = 0;
+		boolean found = false;
+		while (i < users.size())
+		{
+			String userSearch = users.get(i).name.toLowerCase();
+			if (userSearch.equals(searchName)) {
+				users.remove(i);
+				found = true;
+			} else {
+				i++;
+			}
+		}
+
+		if (!found) {
+			System.out.print("no hay usuarios con ese nombre ");
+			sc.close();
+			return;
+		}
+
+		i = 0;
+		while (i < users.size())
+		{
+			users.get(i).hello();
+			i++;
+		}
+		sc.close();
+	}
 }
