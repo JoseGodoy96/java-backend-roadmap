1061947b2f6f88118c367c8df836e9b5620c6ba9
Jose Maria <josemariagodoy1996@gmail.com>
2026-03-21 10:43:33 +0100

    <feat>(User.java, UserDelete.java): Inicio del ejercicio

---
+++ java-basics/07-oop/exercise-5/UserDelete.java
@@ -0,0 +1,48 @@
+/*
+Exercise: Delete User by Name
+
+Goal:
+Practice removing elements from a list of objects.
+
+Instructions:
+
+1. Use the User class.
+
+2. Ask the user how many users they want to enter.
+
+3. Store all users in an ArrayList.
+
+4. Ask the user for a name to delete.
+
+5. Search the list:
+
+	- If a user with that name exists → remove it from the list
+	- If not → print "User not found"
+
+6. After the operation:
+
+	- Print the updated list of users
+
+Example:
+
+Users:
+Ana 20
+Carlos 30
+Marta 25
+
+Delete: Carlos
+
+Result:
+Ana - 20
+Marta - 25
+
+Concepts practiced:
+- removing elements from ArrayList
+- searching in collections
+- loops
+- object comparison
+*/
+
+public class UserDelete {
+	
+}
