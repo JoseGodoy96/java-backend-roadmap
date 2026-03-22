50fe18d4eacf342e2ec0e09ace960571d2f59e00
Jose Maria <josemariagodoy1996@gmail.com>
2026-03-22 19:56:42 +0100

    <feat>(user-api): Inicio de los proyectos con spring boot y nuevo entorno con IntelliJ

---
+++ spring-boot/user-api/src/test/java/com/chema/userapi/UserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chema.userapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
