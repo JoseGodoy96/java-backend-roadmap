888e09cf7fe187856575868d88f02176e249191b
Jose Maria <josemariagodoy1996@gmail.com>
2026-03-24 18:40:39 +0100

    <feat>(user-api): Mejora para usar id

--- spring-boot/user-api/src/main/java/com/chema/userapi/User.java
+++ spring-boot/user-api/src/main/java/com/chema/userapi/User.java
@@ -3,6 +3,10 @@ package com.chema.userapi;
 public class User {
     public String name;
     public int age;
+    public int id;
+
+    public User() {
+    }
 
     public User(String name, int age) {
         this.name = name;
--- spring-boot/user-api/src/main/java/com/chema/userapi/UserController.java
+++ spring-boot/user-api/src/main/java/com/chema/userapi/UserController.java
@@ -10,6 +10,7 @@ import java.util.List;
 public class UserController {
 
     private List<User> users = new ArrayList<>();
+    private int nextId = 1;
 
     // GET /users
     @GetMapping
@@ -20,17 +21,18 @@ public class UserController {
     // POST /users
     @PostMapping
     public String addUser(@RequestBody User user) {
+        user.id = nextId++;
         users.add(user);
         return "User added";
     }
 
-    // DELETE /users/name
-    @DeleteMapping("/{name}")
-    public String deleteUser(@PathVariable String name) {
+    // DELETE /users/{id}
+    @DeleteMapping("/{id}")
+    public String deleteUser(@PathVariable int id) {
 
         int i = 0;
         while (i < users.size()) {
-            if (users.get(i).name.equalsIgnoreCase(name)) {
+            if (users.get(i).id == id) {
                 users.remove(i);
                 return "User deleted";
             }
@@ -40,12 +42,13 @@ public class UserController {
         return "User not found";
     }
 
-    // PUT /users/name
-    @PutMapping("/{name}")
-    public String updateUser(@PathVariable String name, @RequestBody User updatedUser) {
+    // PUT /users/{id}
+    @PutMapping("/{id}")
+    public String updateUser(@PathVariable int id, @RequestBody User updatedUser) {
 
         for (User user : users) {
-            if (user.name.equalsIgnoreCase(name)) {
+            if (user.id == id) {
+                user.name = updatedUser.name;
                 user.age = updatedUser.age;
                 return "User updated";
             }
