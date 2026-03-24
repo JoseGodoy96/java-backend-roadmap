d048792989175d2f6d29000ef86594d23b831147
Jose Maria <josemariagodoy1996@gmail.com>
2026-03-24 19:15:38 +0100

    <feat>(user-api): Inicio del nuevo proyecto conexion con base de datos

---
+++ spring-boot/user-api-db/src/main/java/com/chema/db/userapidb/UserApiDbApplication.java
@@ -0,0 +1,13 @@
+package com.chema.db.userapidb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserApiDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserApiDbApplication.class, args);
+    }
+
+}
---
+++ spring-boot/user-api-db/src/test/java/com/chema/db/userapidb/UserApiDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chema.db.userapidb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApiDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
