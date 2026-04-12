d52b8041408bdcefe5c39c161c563fbbce846c7d
Jose Maria <josemariagodoy1996@gmail.com>
2026-04-12 22:19:27 +0100

    <feat>(User.java, UserApiDbApplication.java): Importado la clase table para crear la tabla users me ha dado problema user por lo tanto he cambiado el nombre por incompatibilidad con postgress y he creado logica e imports en UserApi para crear un usuario y comprobarlo

--- spring-boot/user-api-db/src/main/java/com/chema/db/userapidb/UserApiDbApplication.java
+++ spring-boot/user-api-db/src/main/java/com/chema/db/userapidb/UserApiDbApplication.java
@@ -2,6 +2,10 @@ package com.chema.db.userapidb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import com.chema.db.userapidb.repository.UserRepository;
+import com.chema.db.userapidb.model.User;
 
 @SpringBootApplication
 public class UserApiDbApplication {
@@ -10,4 +14,18 @@ public class UserApiDbApplication {
         SpringApplication.run(UserApiDbApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner run(UserRepository userRepository) {
+        return args -> {
+            User user = new User();
+            user.setName("Jose");
+            user.setAge(30);
+
+            userRepository.save(user);
+
+            userRepository.findAll().forEach(u -> {
+                System.out.println(u.getName());
+            });
+        };
+    }
 }
--- spring-boot/user-api-db/src/main/java/com/chema/db/userapidb/model/User.java
+++ spring-boot/user-api-db/src/main/java/com/chema/db/userapidb/model/User.java
@@ -3,8 +3,10 @@ package com.chema.db.userapidb.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
