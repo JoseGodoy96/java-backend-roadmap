0b7049b732e37b5553763716ac1bc604a921e94c
Jose Maria <josemariagodoy1996@gmail.com>
2026-04-13 22:36:05 +0100

    <feat>(UserController.java): Primera ruta GET creada en java con Springboot

--- spring-boot/user-api-db/src/main/java/com/chema/db/userapidb/controller/UserController.java
+++ spring-boot/user-api-db/src/main/java/com/chema/db/userapidb/controller/UserController.java
@@ -3,18 +3,23 @@ package com.chema.db.userapidb.controller;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.chema.db.userapidb.repository.UserRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import java.util.List;
+import com.chema.db.userapidb.model.User;
 
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
 
-    @RestController
-    @RequestMapping("/users")
-    public class UserController {
+    private final UserRepository userRepository;
 
-        private final UserRepository userRepository;
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
-        public UserController(UserRepository userRepository) {
-            this.userRepository = userRepository;
-        }
-
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
     }
 }
