e97f63c2dc89d91f7ff608023b16efc001d7e4aa
Jose Maria <josemariagodoy1996@gmail.com>
2026-04-14 21:37:20 +0100

    <feat>(UserApiDbApplication.java): Limpiando Beam para dejar de imprimir automatico y de crear usuarios

--- spring-boot/user-api-db/src/main/java/com/chema/db/userapidb/UserApiDbApplication.java
+++ spring-boot/user-api-db/src/main/java/com/chema/db/userapidb/UserApiDbApplication.java
@@ -2,30 +2,11 @@ package com.chema.db.userapidb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
-import com.chema.db.userapidb.repository.UserRepository;
-import com.chema.db.userapidb.model.User;
 
 @SpringBootApplication
 public class UserApiDbApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UserApiDbApplication.class, args);
-    }
-
-    @Bean
-    CommandLineRunner run(UserRepository userRepository) {
-        return args -> {
-            User user = new User();
-            user.setName("Jose");
-            user.setAge(30);
-
-            userRepository.save(user);
-
-            userRepository.findAll().forEach(u -> {
-                System.out.println(u.getName());
-            });
-        };
     }
 }
