499cc7ea4cb705e420f05a1cb18b4e1b073b0dba
Jose Maria <josemariagodoy1996@gmail.com>
2026-04-14 21:44:11 +0100

    <feat>(userController.java): Creado la ruta DELETE

--- spring-boot/user-api-db/src/main/java/com/chema/db/userapidb/controller/UserController.java
+++ spring-boot/user-api-db/src/main/java/com/chema/db/userapidb/controller/UserController.java
@@ -8,6 +8,8 @@ import java.util.List;
 import com.chema.db.userapidb.model.User;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 @RestController
@@ -27,4 +29,9 @@ public class UserController {
 
     @PostMapping
     public User createUser(@RequestBody User user) { return userRepository.save(user); }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userRepository.deleteById(id);
+    }
 }
