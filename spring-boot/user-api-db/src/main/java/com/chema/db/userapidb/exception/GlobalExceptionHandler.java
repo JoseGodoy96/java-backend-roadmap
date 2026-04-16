623800a1355b382b178abb9aea366fc43ffb95bb
Jose Maria <josemariagodoy1996@gmail.com>
2026-04-16 20:00:05 +0100

    <feat>(GlobalExceptionHandler.java): logica mensajes responsive al introducir datos inadecuados en el POST

--- spring-boot/user-api-db/src/main/java/com/chema/db/userapidb/exception/GlobalExceptionHandler.java
+++ spring-boot/user-api-db/src/main/java/com/chema/db/userapidb/exception/GlobalExceptionHandler.java
@@ -1,4 +1,24 @@
 package com.chema.db.userapidb.exception;
 
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<String>> handleValidationErrors(MethodArgumentNotValidException ex) {
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> error.getField() + " " + error.getDefaultMessage())
+                .collect(Collectors.toList());
+
+        return ResponseEntity.badRequest().body(errors);
+    }
 }
